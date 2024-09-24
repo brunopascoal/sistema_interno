763c1bfcf7ff11031d74aebc20e633ad5026edfb
KleberCViana <152494879+KleberCViana@users.noreply.github.com>
2024-09-24 13:57:20 -0300

    Update deployment.py

--- app/deployment.py
+++ app/deployment.py
@@ -33,5 +33,6 @@ DATABASES = {
         'HOST': conn_str_params['host'],
         'USER': conn_str_params['user'],
         'PASSWORD': conn_str_params['password'],
+        'PORT': conn_str_params.get('port', '5432'),        
     }
 }
